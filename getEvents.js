46c7193cdd4666cf9ca7d5f877c8846bad76658b
Andre Cronje <andre@mfin.io>
2018-08-24 11:57:05 +0200

    mapLimit
    Signed-off-by: Andre Cronje <andre@mfin.io>

--- getEvents.js
+++ getEvents.js
@@ -1,26 +1,30 @@
 const axios = require('axios');
 const db = require('./helpers/db.js').db;
+const async = require("async");
 
 getConsensusEvents()
 
 function getConsensusEvents() {
   axios.get('http://18.191.184.199:8000/consensusevents/')
   .then((response) => {
-    response.data.map(insertConsensusEvent)
+    async.mapLimit(response, 10, (event, callback) => {insertConsensusEvent(event, callback)}, function(err, results) {
+      setTimeout(getConsensusEvents, 500)
+    });
   })
   .catch((error) => {
     console.log(error);
   });
 }
 
-function insertConsensusEvent(event) {
+function insertConsensusEvent(event, callback) {
   db.none('insert into consensus_events (hash) select ($1) where NOT EXISTS (SELECT hash FROM consensus_events WHERE hash = $1);', [
     event
   ])
   .then(() => {
-    setTimeout(getConsensusEvents, 500)
+    callback(null, true)
   })
   .catch((err) => {
     console.log(err)
+    callback(err, null)
   })
 }
